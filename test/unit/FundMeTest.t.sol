a1b8276b30634945c13c74e2cf95aa68a4d5a39f
Saumyajit <86034982+AgSpades@users.noreply.github.com>
2025-03-09 12:17:25 +0530

    feat: add interaction scripts and integration tests for FundMe contract

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {FundMe} from "../src/FundMe.sol";
+import {FundMe} from "../../src/FundMe.sol";
 import {Test, console} from "forge-std/Test.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
